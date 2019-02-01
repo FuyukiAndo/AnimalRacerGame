5d549ec3790a236053ef8a35a065d10d46955872
Hamplox <zorkan.p@gmail.com>
2019-02-01 16:55:49 +0100

    Mås Movement 1.0

---
+++ AnimalThingy/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+
+
+    public int speed;
+    public int JumpStrenght;
+    public float distanceBeforeFlight = 100;
+    public float glidGravity = 0.1f;
+    public LayerMask groundLayer;
+
+
+    private Rigidbody2D rb2d;
+    private BoxCollider2D bc2d;
+    private Vector2 player;
+    private float distanceToGround;
+    private bool isGrounded;
+    public float currentDistanceToFlight;
+
+
+
+
+    void Start () {
+        rb2d = GetComponent<Rigidbody2D>();
+        bc2d = GetComponent<BoxCollider2D>();
+        currentDistanceToFlight = 0;
+        distanceToGround = bc2d.bounds.extents.y;
+        
+	}
+	void FixedUpdate () {
+        Debug.DrawRay(transform.position, Vector3.down * (distanceToGround + 1f), Color.red);
+        ifGrouded();
+        HorizontalMovement();
+        VerticalMovement();
+    }
+    void ifGrouded()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, distanceToGround + 1f, groundLayer);
+        if (hit.collider != null)
+        {
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false;
+        }
+    }
+    void HorizontalMovement()
+    {
+        if (Input.GetKey("a") && isGrounded == true)
+        {
+            player = new Vector2(-speed, rb2d.velocity.y);
+            rb2d.velocity = player;
+            currentDistanceToFlight += rb2d.velocity.x;
+        }
+        else if (Input.GetKey("d") && isGrounded == true)
+        {
+            player = new Vector2(speed, rb2d.velocity.y);
+            rb2d.velocity = player;
+            currentDistanceToFlight += rb2d.velocity.x;
+        }
+        else if (Input.GetKey("a") && isGrounded == false)
+        {
+            player = new Vector2(-speed, rb2d.velocity.y);
+            rb2d.velocity = player;
+        }
+        else if (Input.GetKey("d") && isGrounded == false)
+        {
+            player = new Vector2(speed, rb2d.velocity.y);
+            rb2d.velocity = player;
+        }
+        else
+        {
+            player = new Vector2(0, rb2d.velocity.y);
+            rb2d.velocity = player;
+            currentDistanceToFlight = 0;
+        }
+    }
+    void VerticalMovement()
+    {
+        if (currentDistanceToFlight > distanceBeforeFlight || currentDistanceToFlight < -distanceBeforeFlight)
+        {
+            if (Input.GetKey("w") && isGrounded == true)
+            {
+                rb2d.AddForce(new Vector2(0, JumpStrenght));
+                currentDistanceToFlight = 0;    
+            }
+        }
+        if(Input.GetKey("w") && isGrounded == false && rb2d.velocity.y < 0)
+        {
+            rb2d.velocity = new Vector2(rb2d.velocity.x, -glidGravity);
+        }
+    }
+
+}
