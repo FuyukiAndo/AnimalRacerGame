c7fef89c3e27f1987b4fb3ede0bf4aced92f1e09
Zenzyde <30443090+Zenzyde@users.noreply.github.com>
2019-02-05 15:56:57 +0100

    Added StartManager
    Decently flexible script to control players at the start of the game before countdown is finished & then release them.

---
+++ AnimalThingy/Assets/Scripts/StartManager.cs
@@ -0,0 +1,99 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartManager : MonoBehaviour
+{
+
+	public static StartManager Instance
+	{
+		get
+		{
+			return instance;
+		}
+	}
+
+	private static StartManager instance;
+	[SerializeField] private GameObject[] unplacedPlayers;
+	[SerializeField] private string playerMoveScript;
+	public int TimeUntilStart
+	{
+		get
+		{
+			return timeUntilStart;
+		}
+		set
+		{
+			timeUntilStart = value;
+		}
+	}
+	[SerializeField] private int timeUntilStart;
+	[SerializeField] private Vector2[] playerStartPositions;
+	[SerializeField] private bool startCountDownOnSceneLoad;
+
+	void Start()
+	{
+		if (instance == null)
+		{
+			instance = this;
+		}
+		else if (instance != this)
+		{
+			Destroy(gameObject);
+		}
+		PlacePlayers();
+		TrapPlayers();
+		if (startCountDownOnSceneLoad)
+		{
+			StartCoroutine(CountDownStart());
+		}
+	}
+
+	IEnumerator CountDownStart()
+	{
+		while (timeUntilStart > 0)
+		{
+			timeUntilStart -= 1;
+			yield return new WaitForSeconds(1f);
+		}
+		ReleasePlayers();
+	}
+
+	void ReleasePlayers()
+	{
+		//string componentName = playerMoveScript.GetType().ToString();
+		foreach (var player in unplacedPlayers)
+		{
+			MonoBehaviour script = player.GetComponent(playerMoveScript)as MonoBehaviour;
+			script.enabled = true;
+			if (player.GetComponent<Rigidbody2D>())
+			{
+				player.GetComponent<Rigidbody2D>().isKinematic = false;
+			}
+		}
+	}
+
+	void TrapPlayers()
+	{
+		//string componentName = playerMoveScript.GetType().ToString();
+		foreach (var player in unplacedPlayers)
+		{
+			MonoBehaviour script = player.GetComponent(playerMoveScript)as MonoBehaviour;
+			script.enabled = false;
+			if (player.GetComponent<Rigidbody2D>())
+			{
+				player.GetComponent<Rigidbody2D>().isKinematic = true;
+			}
+		}
+	}
+
+	void PlacePlayers()
+	{
+		int place = 0;
+		foreach (var player in unplacedPlayers)
+		{
+			player.transform.position = playerStartPositions[place];
+			place++;
+		}
+	}
+}
