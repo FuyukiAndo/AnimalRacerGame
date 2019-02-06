4d00a40e485dea7916cd3e91e3dac3e4d88d6efc
Xiigor <47105441+Xiigor@users.noreply.github.com>
2019-02-06 17:06:52 +0100

    Pause/Start - menu

---
+++ AnimalThingy/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[System.Serializable]
+public class MainMenuOptions
+{
+    public GameObject gameObject;
+}
+
+public class MainMenu : MonoBehaviour {
+
+public MainMenuOptions[] mainMenuOptions;
+
+    public void ExitGame()
+    {
+        Debug.Log("Exit!");
+        Application.Quit();
+    }
+
+    public void DisableCurrentMenu()
+    {
+        for (int i = 0; i < mainMenuOptions.Length; i++)
+        {
+            mainMenuOptions[i].gameObject.SetActive(false);
+        }
+    }
+}
+
