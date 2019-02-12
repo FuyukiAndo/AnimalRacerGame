3d5f3d3677e54addf0d54fad3c23daaf0587c7a9
Hamplox <46965107+Hamplox@users.noreply.github.com>
2019-02-12 11:29:46 +0100

    Bubble

---
+++ AnimalThingy/Assets/Needles.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Needles : FlyingTrajectory
+{
+   void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        rb2d.bodyType = RigidbodyType2D.Kinematic;
+        rb2d.useFullKinematicContacts = true;
+        MoveNeedle();
+    }
+
+    void MoveNeedle()
+    {
+        rb2d.velocity = new Vector2(speed, 0);
+    }
+
+
+
+}
