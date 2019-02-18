61da05741b8dbc207ff759e47f8cbf6a85ed8918
Xiigor <47105441+Xiigor@users.noreply.github.com>
2019-02-18 12:07:50 +0100

    choffe

--- AnimalThingy/Assets/ChoffesScripts/CharacterUIManager.cs
+++ AnimalThingy/Assets/ChoffesScripts/CharacterUIManager.cs
@@ -15,6 +15,8 @@ public class PlayerUI
 
 public class CharacterUIManager : MonoBehaviour {
 
+    public float energyRegMultiplier;
+
     private PlayerUI player1UI;
     private PlayerUI player2UI;
     private PlayerUI player3UI;
@@ -51,18 +53,22 @@ public class CharacterUIManager : MonoBehaviour {
         if(player1UI.player != null)
         {
             player1UI.playerUIObject = GameObject.Find("Player1UI");
+            player1UI.playerUIObject.SetActive(true);
         }
         if (player2UI.player != null)
         {
             player2UI.playerUIObject = GameObject.Find("Player2UI");
+            player2UI.playerUIObject.SetActive(true);
         }
         if (player3UI.player != null)
         {
             player3UI.playerUIObject = GameObject.Find("Player3UI");
+            player3UI.playerUIObject.SetActive(true);
         }
         if (player4UI.player != null)
         {
             player4UI.playerUIObject = GameObject.Find("Player4UI");
+            player4UI.playerUIObject.SetActive(true);
         }
 
     }
@@ -74,26 +80,36 @@ public class CharacterUIManager : MonoBehaviour {
             UpdateEnergy(player);
             AddEnergy(player);
             UpdateSlider(player);
+            SetCurrentCheckpointProgress(player);
         }
 
     }
 
     private void UpdateEnergy(PlayerUI player)
     {
-        if(player.playerEnergy > 100)
+        if(player.playerEnergy >= 100)
         {
             player.playerEnergy = 100;
-            
+            //check input??
+            //run abilityFunction in playerscript
         }
+
         //abilitykey check -> getComponent().keyisdownbool
     }
     private void AddEnergy(PlayerUI player)
     {
         if(player.playerEnergy <= 100)
-        player.playerEnergy += Time.deltaTime;
+        player.playerEnergy += Time.deltaTime * energyRegMultiplier;
     }
     private void UpdateSlider(PlayerUI player)
     {
         player.playerSlider.value = player.playerEnergy;
     }
+
+    private void SetCurrentCheckpointProgress(PlayerUI player)
+    {
+        //get checkpoint tracker
+       // player.playerCheckpoints.text = /*playercheckpointtracker.currentprogress*/ + "/" + /*playercheckpointtracker.total checkpoints
+    }
+
 }
