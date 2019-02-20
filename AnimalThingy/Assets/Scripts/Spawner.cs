f8a86832eb081aa92e0195d45e2e7ecb0212faca
SweFish <47143724+SweFish@users.noreply.github.com>
2019-02-20 12:44:57 +0100

    Ok

---
+++ AnimalThingy/Assets/Scripts/Spawner.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour{
+
+    public GameObject spawnObject;
+    public float timeBetweenSpawns;
+
+    protected float spawnClock;
+
+    protected void Start()
+    {
+        timeBetweenSpawns = Mathf.Clamp(timeBetweenSpawns, 0, timeBetweenSpawns);
+    }
+    protected void SpawnObject()
+    {
+        if (spawnClock > timeBetweenSpawns)
+        {
+
+            Instantiate(spawnObject, transform.position, new Quaternion(0, 0, 0, 0), gameObject.transform);
+            spawnClock = 0;
+        }
+        else
+        {
+            spawnClock += Time.deltaTime;
+        }
+    }
+    protected void spawnAfter(bool first)
+    {
+        if (transform.childCount > 0 && first == true)
+        {
+            spawnClock = 0;
+        }
+    }
+}
