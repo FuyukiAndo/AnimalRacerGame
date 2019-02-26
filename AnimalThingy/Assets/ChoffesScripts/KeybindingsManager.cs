2b417eece3aa4d726ebbd99b59863bb310ce7457
Xiigor <47105441+Xiigor@users.noreply.github.com>
2019-02-26 17:32:49 +0100

    26/2 Choffe

---
+++ AnimalThingy/Assets/ChoffesScripts/KeybindingsManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class PlayerKeybindings
+{
+    public KeyCode left;
+    public KeyCode right;
+    public KeyCode jump;
+    public KeyCode ability;
+}
+
+public class KeybindingsManager : MonoBehaviour {
+
+    public static KeybindingsManager Instance { get; private set; }
+
+    public PlayerKeybindings Player1Keys;
+    public PlayerKeybindings Player2Keys;
+    public PlayerKeybindings Player3Keys;
+    public PlayerKeybindings Player4Keys;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+        Player1Keys = new PlayerKeybindings();
+        Player2Keys = new PlayerKeybindings();
+        Player3Keys = new PlayerKeybindings();
+        Player4Keys = new PlayerKeybindings();
+    }
+
+
+}
---
+++ AnimalThingy/Assets/ChoffesScripts/KeybindingsMenu.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeybindingsMenu : MonoBehaviour {
+
+    public GameObject[] keybindingWindows;
+
+    private void OnEnable()
+    {
+        foreach(GameObject window in keybindingWindows)
+        {
+            window.SetActive(false);
+        }
+        keybindingWindows[0].SetActive(true);
+    }
+
+    public void DisableOtherWindows()
+    {
+        foreach (GameObject window in keybindingWindows)
+        {
+            window.SetActive(false);
+        }
+    }
+
+    public void SetWindowActive(int index)
+    {
+        keybindingWindows[index].SetActive(true);
+    }
+}
