32903fd655e8daf176ef7f85d17755167bd69aa6
Hamplox <zorkan.p@gmail.com>
2019-03-04 10:56:10 +0100

    04/03
    Moving Script from Temporary file to Actual file

---
+++ AnimalThingy/Assets/Scripts/SpawnTerrainSnowball.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnTerrainSnowball : Spawner {
+
+    
+	// Update is called once per frame
+	void Update () {
+        SpawnObject();
+	}
+}
