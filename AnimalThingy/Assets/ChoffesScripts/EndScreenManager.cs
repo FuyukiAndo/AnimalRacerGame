ab5f90577200d3e1ec569977a95b42a5ef711f90
Xiigor <47105441+Xiigor@users.noreply.github.com>
2019-03-05 17:52:21 +0100

    5/3 Choffe VictoryScene

--- AnimalThingy/Assets/ChoffesScripts/EndScreenManager.cs
+++ AnimalThingy/Assets/ChoffesScripts/EndScreenManager.cs
@@ -12,6 +12,7 @@ public class ScoreScreenChild
     public GameObject parent;
     public TextMeshProUGUI timeText;
     public TextMeshProUGUI nameText;
+    public TextMeshProUGUI scoreText;
 }
     public class EndScreenManager : MonoBehaviour {
 
@@ -20,6 +21,8 @@ public class ScoreScreenChild
     public ScoreScreenChild thirdChild;
     public ScoreScreenChild fourthChild;
 
+    public bool victoryScene = false;
+
     private List<ScoreScreenChild> childObjects;
 
     public Color player1Color = Color.red;
@@ -27,13 +30,25 @@ public class ScoreScreenChild
     public Color player3Color = Color.green;
     public Color player4Color = Color.yellow;
 
+    private Dictionary<string, float> playerTimeList = new Dictionary<string, float>();
+    private Dictionary<string, int> playerScoreList = new Dictionary<string, int>();
+
     private void OnEnable()
     {
         AddChildrenToList();
         EnableChildObjects();
-        AddNamesToChildObjects();
-        AddTimeToChildObjects();
-        SetBackgroundColor();
+        if (victoryScene)
+        {
+            AddScoresInVictoryScene();
+            AddTimeInVictoryScene();
+            SortPlayersInVictoryScene();
+        }
+        else
+        {
+            AddNamesToChildObjects();
+            AddTimeToChildObjects();
+            SetBackgroundColor();
+        }
     }
 
     private void AddChildrenToList()
@@ -190,23 +205,106 @@ public class ScoreScreenChild
                     continue;
                 }
             }
-            //    switch (GoalManager.Instance.PlacedPlayers[i].name)
-            //{
-            //    case "Player1":
+        }
+    }
 
-            //        break;
+    private void AddTimeInVictoryScene()
+    {
+        foreach (Player player in InformationManager.Instance.players)
+        {
+            float combinedTime = 0f;
+            combinedTime = player.level1Time + player.level2Time + player.level3Time + player.level4Time;
+            if(player == InformationManager.Instance.player1)
+            {
+                playerTimeList.Add("Player1", combinedTime);
+            }
+            if(player == InformationManager.Instance.player2)
+            {
+                playerTimeList.Add("Player2", combinedTime);
+            }
+            if (player == InformationManager.Instance.player3)
+            {
+                playerTimeList.Add("Player3", combinedTime);
+            }
+            if (player == InformationManager.Instance.player4)
+            {
+                playerTimeList.Add("Player4", combinedTime);
+            }
+        }
+    }
 
-            //    case "Player2":
+    private void AddScoresInVictoryScene()
+    {
+        foreach (Player player in InformationManager.Instance.players)
+        {
+            if (player == InformationManager.Instance.player1)
+            {
+                playerScoreList.Add("Player1", InformationManager.Instance.player1.score);
+            }
+            if (player == InformationManager.Instance.player2)
+            {
+                playerScoreList.Add("Player2", InformationManager.Instance.player2.score);
+            }
+            if (player == InformationManager.Instance.player3)
+            {
+                playerScoreList.Add("Player3", InformationManager.Instance.player3.score);
+            }
+            if (player == InformationManager.Instance.player4)
+            {
+                playerScoreList.Add("Player4", InformationManager.Instance.player4.score);
+            }
+        }
+    }
 
-            //        break;
+    private void SortPlayersInVictoryScene()
+    {
+        List<float> sortedPlayerTimes = new List<float>();
+        foreach(var player in playerTimeList)
+        {
+            sortedPlayerTimes.Add(player.Value);
+        }
+        sortedPlayerTimes.Sort();
+        List<int> sortedPlayerScores = new List<int>();
+        foreach (var player in playerScoreList)
+        {
+            sortedPlayerScores.Add(player.Value);
+        }
+        sortedPlayerScores.Sort();
+        sortedPlayerScores.Reverse();
 
-            //    case "Player3":
 
-            //        break;
-            //    case "Player4":
 
-            //        break;
-            //}
+        for(int i = 0; i < childObjects.Count; i++)
+        {
+            if(sortedPlayerScores[i] == InformationManager.Instance.player1.score && childObjects[i].nameText.text == null)
+            {
+                childObjects[i].nameText.text = "Player1";
+                childObjects[i].timeText.text = playerTimeList["Player1"].ToString();
+                childObjects[i].scoreText.text = sortedPlayerScores[i].ToString();
+                goto END;
+            }
+            if (sortedPlayerScores[i] == InformationManager.Instance.player2.score && childObjects[i].nameText.text == null)
+            {
+                childObjects[i].nameText.text = "Player2";
+                childObjects[i].timeText.text = playerTimeList["Player2"].ToString();
+                childObjects[i].scoreText.text = sortedPlayerScores[i].ToString();
+                goto END;
+            }
+            if (sortedPlayerScores[i] == InformationManager.Instance.player3.score && childObjects[i].nameText.text == null)
+            {
+                childObjects[i].nameText.text = "Player3";
+                childObjects[i].timeText.text = playerTimeList["Player3"].ToString();
+                childObjects[i].scoreText.text = sortedPlayerScores[i].ToString();
+                goto END;
+            }
+            if (sortedPlayerScores[i] == InformationManager.Instance.player4.score && childObjects[i].nameText.text == null)
+            {
+                childObjects[i].nameText.text = "Player4";
+                childObjects[i].timeText.text = playerTimeList["Player4"].ToString();
+                childObjects[i].scoreText.text = sortedPlayerScores[i].ToString();
+                goto END;
+            }
+        END:;
         }
     }
 
@@ -227,5 +325,10 @@ public class ScoreScreenChild
                 gameObject.GetComponent<Image>().color = player4Color;
                 break;
         }
+    }
+
+    public void Continue()
+    {
+        SceneManager.LoadScene("StartMenu");
     }
 }
