762762b58f7ff16cef6eecb86f534468f87c554d
Hamplox <zorkan.p@gmail.com>
2019-03-13 00:48:54 +0100

    Small changes

--- AnimalThingy/Assets/Scripts/PeterScript/TerrainSnowball.cs
+++ AnimalThingy/Assets/Scripts/PeterScript/TerrainSnowball.cs
@@ -6,14 +6,19 @@ using UnityEngine.Animations;
 public class TerrainSnowball : MonoBehaviour {
 
     public float speed = 5.0f;
-    public LayerMask terrainLayer;
+    public LayerMask terrainLayer, characterLayer;
+    public float pushForce = 5.0f;
+    public float stunDuration;
 
     private Rigidbody2D rb2d;
+    private Collider2D c2d;
+    private Collider2D[] collision;
     private float dir;
 
 	// Use this for initialization
 	void Start () {
         rb2d = GetComponent<Rigidbody2D>();
+        c2d = GetComponent<Collider2D>();
         if(speed > 0)
         {
             dir = gameObject.transform.lossyScale.x;
@@ -27,11 +32,25 @@ public class TerrainSnowball : MonoBehaviour {
 	// Update is called once per frame
 	void FixedUpdate () {
         Debug.DrawRay(transform.position,Vector2.right * (dir), Color.red);
-        hitWall();
+        HitWall();
+        HitPlayer();
         Vector2 movement = new Vector2(speed, 0);
         rb2d.AddForce(movement);
 	}
-    public void hitWall()
+    public void HitPlayer()
+    {
+        collision = Physics2D.OverlapBoxAll(transform.position, c2d.bounds.size, 0.0f);
+        foreach(var collider in collision)
+        {
+            bool isOnLayer = characterLayer == (characterLayer | (1 << collider.gameObject.layer));
+            if (isOnLayer)
+            {
+                collider.GetComponent<PlayerInput>().stunDurationLeft = stunDuration;
+                collider.GetComponent<PlayerController>().movement.x += rb2d.velocity.x * pushForce;
+            }
+        }
+    }
+    public void HitWall()
     {
         RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.right, dir, terrainLayer);
         if(hit.collider != null)
