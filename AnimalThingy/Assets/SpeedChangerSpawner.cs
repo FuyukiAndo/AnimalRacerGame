4e8ed5d73d6365efb1ea3bf1b45715b5e97a5277
Hamplox <zorkan.p@gmail.com>
2019-03-13 17:25:21 +0100

    MushroomSpawner
    goes by the name  MushroomSpawner

---
+++ AnimalThingy/Assets/SpeedChangerSpawner.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeedChangerSpawner : MonoBehaviour {
+
+    public GameObject goodMushroom;
+    [Range(0, 1)]
+    public float chanceForGoodMushroom;
+    public float goodMushroomSpeed;
+
+    public GameObject badMushroom;
+    [Range(0, 1)]
+    public float chanceForBadMushroom;
+    public float badMushroomSpeed;
+
+    public bool wantSpawnerToBeRemoved;
+    private int chance;
+    private float spawnChanceAmountOnGood;
+    private void Awake()
+    {
+        if (wantSpawnerToBeRemoved == true)
+        {
+            if (GetComponent<MeshRenderer>() != null)
+            {
+                GetComponent<MeshRenderer>().enabled = false;
+            }
+            if (GetComponent<Collider2D>() != null)
+            {
+                GetComponent<Collider2D>().enabled = false;
+            }
+        }
+        goodMushroomSpeed = Mathf.Clamp(goodMushroomSpeed, 0, Mathf.Infinity);
+        badMushroomSpeed = Mathf.Clamp(badMushroomSpeed, -Mathf.Infinity, 0);
+        chanceForBadMushroom = 1 - chanceForGoodMushroom;
+        
+    }
+    private void Start()
+    {
+        chance = Random.Range(0, 100);
+        spawnChanceAmountOnGood = chanceForGoodMushroom * 100;
+        Debug.Log(spawnChanceAmountOnGood);
+        if (chance < spawnChanceAmountOnGood)
+        {
+            Instantiate(goodMushroom, transform.position, new Quaternion(0, 0, 0, 0), gameObject.transform);
+        }
+        else
+        {
+            Instantiate(badMushroom, transform.position, new Quaternion(0, 0, 0, 0), gameObject.transform);
+        }
+    }
+    public float getGoodSpeed
+    {
+        get { return goodMushroomSpeed; }
+    }
+    public float getBadSpeed
+    {
+        get { return badMushroomSpeed; }
+    }
+}
